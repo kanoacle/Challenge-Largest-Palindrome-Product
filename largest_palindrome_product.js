fc8e91193451afd6117812536387be3129b85d4e
Kanojah <kanoacleveland@Kanojahs-MacBook-Pro.local>
2017-03-17 10:52:42 -1000

    help from charles

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -1,18 +1,30 @@
-/*  function(digits)
- *
- *  @param {Number} digits         the amount of digits in each multiplicand
- *
- *  @return {Object} an object containing the two factors used to produce
- *                   the palindromeNumber and the palindromeNumber itself.
- */
 module.exports = function(digits){
+  var maxDigit = Math.pow(10, digits);
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
 
-  // do your work here
+  function palindromeCheck (number) {
+    number = number.toString();
+    var reverseNum = number.split("").reverse().join("");
+    if (number === reverseNum) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-
+  for (var x = maxDigit / 10; x < maxDigit; x++) {
+      for (var y = maxDigit / 10; y < maxDigit; y++) {
+        if (palindromeCheck(x * y) === true) {
+          if (x * y > palindromeNumber) {
+            factor_0 = x;
+            factor_1 = y;
+            palindromeNumber = factor_0 * factor_1;
+          }
+        }
+      }
+    }
 
   return {
     factor_0 : factor_0,
